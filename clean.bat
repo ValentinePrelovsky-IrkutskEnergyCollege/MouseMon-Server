08553d5e4f6bcd50a089ecc41f63a9591ac8fec6
Valentin <prelovskyvalentine@gmail.com>
2017-03-10 12:26:51 +0800

    Absolute path for cleaning

--- clean.bat
+++ clean.bat
@@ -1,3 +1,4 @@
-del log.txt
-del Server.exe
-rename Project1.exe Server.exe
+del "C:\Users\Valentin\Desktop\srv\log.txt"
+del "C:\Users\Valentin\Desktop\srv\Server.exe"
+
+rename "C:\Users\Valentin\Desktop\srv\Project1.exe" "Server.exe"
